0448bda45aaec4bffc48c4af65723751fcb87f2f
Lokesh Saboo <lokesh.sabu@gmail.com>
2025-03-09 10:10:48 +0530

    resume api corrected

--- src/main/java/com/ashv/ats/resumebuilder/controller/ResumeController.java
+++ src/main/java/com/ashv/ats/resumebuilder/controller/ResumeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/resume")
+@RequestMapping("/resumebuilder/resume")
 public class ResumeController {
 
     private final ResumeService resumeService;
